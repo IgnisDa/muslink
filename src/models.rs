41771b450d95c7f7009837827d354498fc335428
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-01-19 00:43:07 +0530

    chore(backend): add user country support to input

--- src/models.rs
+++ src/models.rs
@@ -3,4 +3,6 @@ use async_graphql::InputObject;
 #[derive(InputObject, Debug)]
 pub struct ResolveMusicLinkInput {
     pub link: String,
+    #[graphql(default = "US")]
+    pub user_country: String,
 }
