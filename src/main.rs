02968b6b5311d79297a3452ff4b9045dcfb98b5c
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-01-19 00:20:13 +0530

    feat(backend): add basic tracing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema};
 use async_graphql_axum::GraphQL;
 use axum::{
@@ -7,6 +8,7 @@ use axum::{
 };
 use resolver::QueryRoot;
 use tokio::net::TcpListener;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod resolver;
 
@@ -15,12 +17,21 @@ async fn graphiql() -> impl IntoResponse {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| format!("{}=debug", env!("CARGO_CRATE_NAME")).into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
 
     let app = Router::new().route("/", get(graphiql).post_service(GraphQL::new(schema)));
 
-    axum::serve(TcpListener::bind("127.0.0.1:5000").await.unwrap(), app)
-        .await
-        .unwrap();
+    let listener = TcpListener::bind(format!("0.0.0.0:5000")).await.unwrap();
+    tracing::info!("Listening on {}", listener.local_addr()?);
+    axum::serve(listener, app).await.unwrap();
+    Ok(())
 }
