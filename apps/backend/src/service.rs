c162aecc3ec597dc825efe5acf34baae1f711aef
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-01-19 10:37:10 +0530

    feat(backend): do not fail if incorrect country specified

--- apps/backend/src/service.rs
+++ apps/backend/src/service.rs
@@ -1,6 +1,6 @@
-use async_graphql::{Error, Result};
+use async_graphql::Result;
 use reqwest::{Client, Url};
-use rust_iso3166::from_alpha2;
+use rust_iso3166::{from_alpha2, US};
 use strum::IntoEnumIterator;
 
 use crate::{
@@ -32,15 +32,14 @@ impl Service {
     ) -> Result<ResolveMusicLinkResponse> {
         tracing::debug!("Received link: {:?}", input);
 
-        from_alpha2(input.user_country.as_str())
-            .ok_or_else(|| Error::new("Invalid country code"))?;
+        let user_country = from_alpha2(input.user_country.as_str()).unwrap_or(US);
 
         let url = Url::parse_with_params(
             SONG_LINK_API_URL,
             &[
                 ("songIfSingle", "true"),
                 ("url", input.link.as_str()),
-                ("userCountry", input.user_country.as_str()),
+                ("userCountry", user_country.alpha2),
             ],
         )?;
         let response = self
