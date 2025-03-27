fc6275504cf4be619a55b15231cc44b5e24d8ca7
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-03-27 09:01:47 +0530

    feat: implement configuration loading for telegram-bot using schematic

--- apps/telegram-bot/src/main.rs
+++ apps/telegram-bot/src/main.rs
@@ -1,5 +1,13 @@
 use graphql_client::GraphQLQuery;
+use schematic::{Config, ConfigLoader, validate::not_empty};
 use teloxide::prelude::*;
+
+#[derive(Config)]
+#[config(env)]
+struct AppConfig {
+    #[setting(validate = not_empty, env = "TELOXIDE_TOKEN")]
+    teloxide_token: String,
+}
 
 #[derive(GraphQLQuery)]
 #[graphql(
@@ -13,7 +21,9 @@ struct ResolveMusicLink;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenvy::dotenv()?;
 
-    let bot = Bot::from_env();
+    let config = ConfigLoader::<AppConfig>::new().load()?.config;
+
+    let bot = Bot::new(config.teloxide_token);
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
         bot.send_message(msg.chat.id, "Hello, world!").await?;
