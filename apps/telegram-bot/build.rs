eaa9cef9ee2d01e80aca7c6cfd5ae6d02306cd4a
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-03-27 08:45:28 +0530

    Revert "chore: update Cargo.toml and Cargo.lock for telegram-bot, upgrading dependencies and adding new packages"
    This reverts commit da5065a6ac7a6988995bcb86eaebba36ba018a10.

--- apps/telegram-bot/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    cynic_codegen::register_schema("backend-schema")
-        .from_sdl_file("../../libs/generated/backend-schema.graphql")
-        .unwrap()
-        .as_default()
-        .unwrap();
-}
