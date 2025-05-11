d4aef594c78ec6e6f60355d20615e948123a8f3f
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-05-11 20:10:36 +0530

    chore(migrations): create music link

--- libs/migrations/src/lib.rs
+++ libs/migrations/src/lib.rs
@@ -3,6 +3,7 @@ pub use sea_orm_migration::prelude::*;
 mod m20250511_create_extensions;
 mod m20250512_create_telegram_bot_channel;
 mod m20250513_create_telegram_bot_user;
+mod m20250514_create_music_link;
 
 pub struct Migrator;
 
@@ -13,6 +14,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20250511_create_extensions::Migration),
             Box::new(m20250512_create_telegram_bot_channel::Migration),
             Box::new(m20250513_create_telegram_bot_user::Migration),
+            Box::new(m20250514_create_music_link::Migration),
         ]
     }
 }
---
+++ libs/migrations/src/m20250514_create_music_link.rs
@@ -0,0 +1,72 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[derive(Iden)]
+pub enum MusicLink {
+    Id,
+    Table,
+    CreatedAt,
+    SpotifyLink,
+    AppleMusicLink,
+    YoutubeMusicLink,
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(MusicLink::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(MusicLink::Id)
+                            .uuid()
+                            .not_null()
+                            .primary_key()
+                            .default(PgFunc::gen_random_uuid()),
+                    )
+                    .col(ColumnDef::new(MusicLink::SpotifyLink).text())
+                    .col(ColumnDef::new(MusicLink::AppleMusicLink).text())
+                    .col(ColumnDef::new(MusicLink::YoutubeMusicLink).text())
+                    .col(
+                        ColumnDef::new(MusicLink::CreatedAt)
+                            .timestamp_with_time_zone()
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .create_index(
+                Index::create()
+                    .name("idx_music_link_spotify_link")
+                    .table(MusicLink::Table)
+                    .col(MusicLink::SpotifyLink)
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .create_index(
+                Index::create()
+                    .name("idx_music_link_apple_music_link")
+                    .table(MusicLink::Table)
+                    .col(MusicLink::AppleMusicLink)
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .create_index(
+                Index::create()
+                    .name("idx_music_link_youtube_music_link")
+                    .table(MusicLink::Table)
+                    .col(MusicLink::YoutubeMusicLink)
+                    .to_owned(),
+            )
+            .await?;
+        Ok(())
+    }
+}
