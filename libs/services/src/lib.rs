ae64951d5be89893d9db226cab61f714d1f78daf
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-05-11 20:43:05 +0530

    feat(service): add functionality to retrieve music links from the database

--- libs/services/src/lib.rs
+++ libs/services/src/lib.rs
@@ -1,7 +1,8 @@
 use anyhow::Result;
+use entities::{music_link, prelude::MusicLink};
 use reqwest::{Client, Url};
 use rust_iso3166::{US, from_alpha2};
-use sea_orm::DatabaseConnection;
+use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
 use strum::IntoEnumIterator;
 
 mod models;
@@ -23,12 +24,70 @@ impl MusicLinkService {
         Self { client }
     }
 
+    async fn get_music_link_from_db(
+        &self,
+        link: &String,
+        db: &DatabaseConnection,
+    ) -> Result<Option<music_link::Model>> {
+        let music_link = MusicLink::find()
+            .filter(
+                music_link::Column::SpotifyLink
+                    .eq(link)
+                    .or(music_link::Column::AppleMusicLink.eq(link))
+                    .or(music_link::Column::YoutubeMusicLink.eq(link)),
+            )
+            .one(db)
+            .await?;
+        Ok(music_link)
+    }
+
     pub async fn resolve_music_link(
         &self,
         input: MusicLinkInput,
         db: &DatabaseConnection,
     ) -> Result<MusicLinkResponse> {
         tracing::debug!("Received link: {:?}", input);
+
+        let music_link = self.get_music_link_from_db(&input.link, db).await?;
+        if let Some(music_link) = music_link {
+            tracing::debug!("Found music link in db: {:?}", music_link);
+            let mut found = 0;
+            let mut collected_links = vec![];
+            if let Some(spotify_link) = music_link.spotify_link {
+                found += 1;
+                collected_links.push(MusicLinkData {
+                    platform: MusicPlatform::Spotify,
+                    data: Some(MusicLinkPlatformData {
+                        id: music_link.id,
+                        url: spotify_link,
+                    }),
+                });
+                if let Some(apple_music_link) = music_link.apple_music_link {
+                    found += 1;
+                    collected_links.push(MusicLinkData {
+                        platform: MusicPlatform::AppleMusic,
+                        data: Some(MusicLinkPlatformData {
+                            id: music_link.id,
+                            url: apple_music_link,
+                        }),
+                    });
+                }
+                if let Some(youtube_music_link) = music_link.youtube_music_link {
+                    found += 1;
+                    collected_links.push(MusicLinkData {
+                        platform: MusicPlatform::YoutubeMusic,
+                        data: Some(MusicLinkPlatformData {
+                            id: music_link.id,
+                            url: youtube_music_link,
+                        }),
+                    });
+                }
+            }
+            return Ok(MusicLinkResponse {
+                found,
+                collected_links,
+            });
+        }
 
         let user_country = from_alpha2(input.user_country.as_str()).unwrap_or(US);
 
