7cc6610088fb5e9af8592edb6fda5b2172de08c2
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-05-11 10:43:21 +0530

    refactor: data access

--- apps/telegram-bot/src/services.rs
+++ apps/telegram-bot/src/services.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashSet, sync::Arc};
 
-use crate::entities::telegram_bot_channel;
+use crate::entities::{prelude::TelegramBotChannel, telegram_bot_channel};
 use convert_case::{Case, Casing};
 use regex::Regex;
 use sea_orm::{
@@ -16,7 +16,7 @@ pub async fn find_or_create_channel(
     db: &DatabaseConnection,
     telegram_channel_id: i64,
 ) -> Result<telegram_bot_channel::Model, DbErr> {
-    let existing_channel = telegram_bot_channel::Entity::find()
+    let existing_channel = TelegramBotChannel::find()
         .filter(telegram_bot_channel::Column::TelegramChannelId.eq(telegram_channel_id))
         .one(db)
         .await?;
