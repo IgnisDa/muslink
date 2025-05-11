25a814a8ede5a69097850b655033f6d3f9d5d1e5
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-05-11 23:06:46 +0530

    feat(model): add all_links field to music link model for improved link management

--- libs/entities/src/music_link.rs
+++ libs/entities/src/music_link.rs
@@ -7,6 +7,7 @@ use sea_orm::entity::prelude::*;
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
     pub id: Uuid,
+    pub all_links: Vec<String>,
     pub created_at: DateTimeUtc,
     pub spotify_link: Option<String>,
     pub equivalent_links: Vec<String>,
