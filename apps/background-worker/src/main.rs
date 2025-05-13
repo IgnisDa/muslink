27107546aa65d5b613f487d26fd43e3b753b5657
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-05-13 19:36:12 +0530

    Implement scheduling functionality in background-worker app
    * Add main function to initialize and run a job scheduler using apalis and apalis-cron.
    * Define Reminder struct and schedule_job function to handle job execution.
    * Configure worker with tracing, load shedding, and cron scheduling capabilities.

--- apps/background-worker/src/main.rs
+++ apps/background-worker/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::str::FromStr;
+
+use apalis::{
+    layers::WorkerBuilderExt,
+    prelude::{Error, Monitor, WorkerBuilder, WorkerFactoryFn},
+};
+use apalis_cron::{CronContext, CronStream, Schedule};
+use chrono::Local;
+use tokio::join;
+use tower::load_shed::LoadShedLayer;
+
+#[derive(Debug, Default)]
+struct Reminder;
+
+async fn schedule_job(_job: Reminder, ctx: CronContext<Local>) -> Result<(), Error> {
+    println!("Performing job {}", ctx.get_timestamp());
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let worker = Monitor::new()
+        .register(
+            WorkerBuilder::new("schedule-job")
+                .enable_tracing()
+                .layer(LoadShedLayer::new())
+                .catch_panic()
+                .backend(CronStream::new_with_timezone(
+                    Schedule::from_str("* * * * * *").unwrap(),
+                    Local,
+                ))
+                .build_fn(schedule_job),
+        )
+        .run();
+
+    let _ = join!(worker);
+
+    Ok(())
 }
