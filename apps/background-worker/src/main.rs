4c1fe49a9cd4ca634e39781cbf957ee02e0818ad
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-05-13 20:19:30 +0530

    Enhance schedule_job function in background-worker app
    * Update schedule_job to accept a database connection as a parameter.
    * Implement logic to fetch unrated TelegramBotMusicShareReaction entries from the database.
    * Add logging for job execution timestamp and debug output for unrated reactions.

--- apps/background-worker/src/main.rs
+++ apps/background-worker/src/main.rs
@@ -2,13 +2,14 @@ use std::str::FromStr;
 
 use apalis::{
     layers::WorkerBuilderExt,
-    prelude::{Error, Monitor, WorkerBuilder, WorkerFactoryFn},
+    prelude::{Data, Error, Monitor, WorkerBuilder, WorkerFactoryFn},
 };
 use apalis_cron::{CronContext, CronStream, Schedule};
 use chrono::Local;
+use entities::{prelude::TelegramBotMusicShareReaction, telegram_bot_music_share_reaction};
 use migrations::MigratorTrait;
 use schematic::{Config, ConfigLoader, validate::not_empty};
-use sea_orm::Database;
+use sea_orm::{ColumnTrait, Database, DatabaseConnection, EntityTrait, QueryFilter};
 use serde::Serialize;
 use tokio::join;
 use tower::load_shed::LoadShedLayer;
@@ -24,10 +25,18 @@ struct AppConfig {
 #[derive(Debug, Default)]
 struct Reminder;
 
-async fn schedule_job(_job: Reminder, ctx: CronContext<Local>) -> Result<(), Error> {
-    tracing::debug!("Starting schedule_job");
-    tracing::info!("Performing job {}", ctx.get_timestamp());
-    tracing::debug!("Finished schedule_job");
+async fn schedule_job(
+    _job: Reminder,
+    ctx: CronContext<Local>,
+    db: Data<DatabaseConnection>,
+) -> Result<(), Error> {
+    tracing::info!("Performing job at: {}", ctx.get_timestamp());
+    let unrated = TelegramBotMusicShareReaction::find()
+        .filter(telegram_bot_music_share_reaction::Column::LlmSentimentAnalysis.is_null())
+        .all(&*db)
+        .await
+        .ok();
+    dbg!(&unrated);
     Ok(())
 }
 
@@ -63,6 +72,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .enable_tracing()
                 .layer(LoadShedLayer::new())
                 .catch_panic()
+                .data(db)
                 .backend(CronStream::new_with_timezone(
                     Schedule::from_str("* * * * * *").unwrap(),
                     Local,
